b8cb04f67525495a8510c932b9bfb9dbd9c3833c
matt <matt@finereport.com>
2016-09-05 10:04:21 +0800

    JAVA设计模式之享元模式
    Signed-off-by: matt <matt@finereport.com>

---
+++ Flyweight/src/com/hyp/FlyweightPattern.java
@@ -0,0 +1,41 @@
+package com.hyp;
+
+import com.hyp.flyweight.Flyweight;
+import com.hyp.flyweight.FlyweightFactory;
+
+/**
+ * Created by hyp on 2016/9/5.
+ */
+public class FlyweightPattern{
+    FlyweightFactory factory = new FlyweightFactory();
+    Flyweight fly1;
+    Flyweight fly2;
+    Flyweight fly3;
+    Flyweight fly4;
+    Flyweight fly5;
+    Flyweight fly6;
+    /** *//** Creates a new instance of FlyweightPattern */
+    public FlyweightPattern(){
+        fly1 = factory.getFlyWeight("Google");
+        fly2 = factory.getFlyWeight("Qutr");
+        fly3 = factory.getFlyWeight("Google");
+        fly4 = factory.getFlyWeight("Google");
+        fly5 = factory.getFlyWeight("Google");
+        fly6 = factory.getFlyWeight("Google");
+    }
+    public void showFlyweight(){
+        fly1.operation();
+        fly2.operation();
+        fly3.operation();
+        fly4.operation();
+        fly5.operation();
+        fly6.operation();
+        int objSize = factory.getFlyweightSize();
+        System.out.println("objSize = " + objSize);
+    }
+    public static void main(String[] args){
+        System.out.println("The FlyWeight Pattern!");
+        FlyweightPattern fp = new FlyweightPattern();
+        fp.showFlyweight();
+    }
+}
---
+++ Flyweight/src/com/hyp/flyweight/Flyweight.java
@@ -0,0 +1,12 @@
+package com.hyp.flyweight;
+
+import java.util.Hashtable;
+
+/**
+ * Created by hyp on 2016/9/5.
+ */
+public abstract class Flyweight{
+    public abstract void operation();
+}
+
+
